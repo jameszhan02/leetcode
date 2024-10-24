5649d175f363ab96d8ad86a8817f3d77068048a1
sillyknight02 <45243472+jameszhan02@users.noreply.github.com>
2024-10-23 22:19:47 -0700

    ignore addded

--- class_note/merge_sort.ts
+++ class_note/merge_sort.ts
@@ -1,6 +1,5 @@
 const mergeSort = (inputArray: number[]) => {
   if (inputArray.length < 2 || !inputArray) return
-
   process(inputArray, 0, inputArray.length)
 }
 
@@ -28,10 +27,24 @@ const merge = (arr: number[], start: number, mid: number, end: number) => {
   let helpIndex = 0
   let pointerLeft = start
   let pointerRight = mid + 1
+  //compare the current pointers and copy the less one into the helper array - before one of them hit the end.
   while (pointerLeft <= mid && pointerRight <= end) {
     helpArr[helpIndex] =
       arr[pointerLeft] <= arr[pointerRight]
         ? arr[pointerLeft++]
         : arr[pointerRight++]
   }
+  while (pointerLeft <= mid) {
+    helpArr[helpIndex++] = arr[pointerLeft++]
+  }
+  while (pointerRight <= end) {
+    helpArr[helpIndex++] = arr[pointerRight++]
+  }
 }
+
+
+const res_merge_sort = mergeSort([3, 55, 77, 23, 65, 86, 224, 24, 1, 0, 52, 11])
+console.log({res_merge_sort});
+
+
+
