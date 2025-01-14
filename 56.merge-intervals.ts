46317036e45819f08ae8f3be63b749ab6d49d6a6
Kara X_X <jameszhan02@hotmail.com>
2025-01-14 14:49:21 -0800

    Update 56.merge-intervals.ts

--- 56.merge-intervals.ts
+++ 56.merge-intervals.ts
@@ -6,26 +6,28 @@
 
 // @lc code=start
 function merge(intervals: number[][]): number[][] {
-    const sortedIntervals = intervals.sort((a, b) => a[0] - b[0])
-    console.log(sortedIntervals);
-
-    const myAns: number[][] = []
-    let mergeSpace: number[] = []
-    for (let i = 0; i < sortedIntervals.length; i++) {
-        if (!(mergeSpace.length > 0)) mergeSpace = sortedIntervals[i]
-        if (!sortedIntervals[i + 1]) {
-            myAns.push(mergeSpace)
-            continue
-        }
-        if (sortedIntervals[i][1] < sortedIntervals[i + 1][0]) {
-            myAns.push(mergeSpace)
-            console.log({ myAns });
-            mergeSpace = []
-        } else {
-            mergeSpace = [mergeSpace[0], Math.max(sortedIntervals[i + 1][1], mergeSpace[1])]
-        }
+  let sortedIntervals = intervals.sort((a, b) => a[0] - b[0])
+  const myAns: number[][] = []
+  let mergeSpace: number[] = []
+  for (let i = 0; i < sortedIntervals.length; i++) {
+    if (!(mergeSpace.length > 0)) {
+      mergeSpace = sortedIntervals[i]
     }
-    return myAns
-};
+    if (!sortedIntervals[i + 1]) {
+      // handle last item |
+      myAns.push(mergeSpace)
+      continue
+    }
+    if (mergeSpace[1] < sortedIntervals[i + 1][0]) {
+      myAns.push(mergeSpace)
+      mergeSpace = []
+    } else {
+      mergeSpace = [
+        mergeSpace[0],
+        Math.max(sortedIntervals[i + 1][1], mergeSpace[1]),
+      ]
+    }
+  }
+  return myAns
+}
 // @lc code=end
-
