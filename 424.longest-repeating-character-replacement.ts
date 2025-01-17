49b5e8edd1bbf8d4d29799a14dc669037bb1f696
Kara X_X <jameszhan02@hotmail.com>
2025-01-16 18:02:28 -0800

    Create 424.longest-repeating-character-replacement.ts

---
+++ 424.longest-repeating-character-replacement.ts
@@ -0,0 +1,9 @@
+/*
+ * @lc app=leetcode id=424 lang=typescript
+ *
+ * [424] Longest Repeating Character Replacement
+ */
+
+// @lc code=start
+function characterReplacement(s: string, k: number): number {}
+// @lc code=end
