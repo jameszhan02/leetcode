ad52dec58fa0f5bf51008559fad44c89e531a1b4
Kara X_X <jameszhan02@hotmail.com>
2025-10-08 17:00:18 -0700

    problem ready to be resolved

---
+++ 128.longest-consecutive-sequence.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=128 lang=cpp
+ *
+ * [128] Longest Consecutive Sequence
+ */
+#include <vector>
+#include <set>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int count = 0;
+        for(int idx = 0; idx < nums.size(); idx++){
+            
+        }
+    }
+};
+// @lc code=end
+
---
+++ 128.longest-consecutive-sequence.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=128 lang=typescript
+ *
+ * [128] Longest Consecutive Sequence
+ */
+
+// @lc code=start
+function longestConsecutive(nums: number[]): number {
+    
+};
+// @lc code=end
+
