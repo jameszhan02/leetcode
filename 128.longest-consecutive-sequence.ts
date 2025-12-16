78dedc3855d1a03a6076870042c80c038c398473
sillyknight02 <45243472+jameszhan02@users.noreply.github.com>
2025-12-16 01:15:24 -0800

    Update 128.longest-consecutive-sequence.ts

--- 128.longest-consecutive-sequence.ts
+++ 128.longest-consecutive-sequence.ts
@@ -6,7 +6,22 @@
 
 // @lc code=start
 function longestConsecutive(nums: number[]): number {
-    
+    if(nums.length == 0) return 0
+    const numsSet = new Set(nums)
+    let longestSequence = 1
+    numsSet.forEach((num)=>{
+        //only start with the start point
+        if(!numsSet.has(num - 1)){ // this is a starting point of a sequence
+            let startingNum = num
+            let currentCount = 1
+            while(numsSet.has(startingNum + 1)){
+                currentCount += 1
+                startingNum += 1
+            }
+            if(currentCount >= longestSequence) longestSequence = currentCount
+        }
+    })
+    return longestSequence
 };
 // @lc code=end
 
