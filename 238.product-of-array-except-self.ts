c0a0c43a7d35a8f87616c53f00cdd5e33bf9448b
Kara X_X <jameszhan02@hotmail.com>
2026-03-17 10:11:30 -0700

    update

--- 238.product-of-array-except-self.ts
+++ 238.product-of-array-except-self.ts
@@ -6,25 +6,24 @@
 
 // @lc code=start
 function productExceptSelf(nums: number[]): number[] {
-    let totalProduct = 1
-    const answer = new Array(nums.length)
-    answer.fill(0)
-    const numsOfZero = nums.filter(num => num === 0).length
-    if (numsOfZero > 1) return answer
-    //!!if there one 0 then we only need to calculate for that index, if 2 0 is every where.
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) continue
-        totalProduct *= nums[i]
-    }
-    if (numsOfZero === 1) {
-        answer[nums.indexOf(0)] = totalProduct
-        return answer
-    }
-    for (let i = 0; i < nums.length; i++) {
-        answer[i] = totalProduct / nums[i]
-    }
-
+  let totalProduct = 1
+  const answer = new Array(nums.length)
+  answer.fill(0)
+  const numsOfZero = nums.filter((num) => num === 0).length
+  if (numsOfZero > 1) return answer
+  //!!if there one 0 then we only need to calculate for that index, if 2 0 is every where.
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) continue
+    totalProduct *= nums[i]
+  }
+  if (numsOfZero === 1) {
+    answer[nums.indexOf(0)] = totalProduct
     return answer
-};
-// @lc code=end
+  }
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = totalProduct / nums[i]
+  }
 
+  return answer
+}
+// @lc code=end
