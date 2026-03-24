c53e36862083cad239739008d6a08637afd7c939
Kara X_X <jameszhan02@hotmail.com>
2026-03-23 17:53:11 -0700

    on going

--- 57.insert-interval.java
+++ 57.insert-interval.java
@@ -5,9 +5,21 @@
  */
 
 // @lc code=start
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        
+        List<int[]> res = new ArrayList<>(Arrays.asList(intervals));
+        for(int i = 0; i < intervals.length; i++){
+            int newStart = newInterval[0];
+            int newEnd = newInterval[1];
+            if(newEnd < intervals[i][0]){
+                res.add(i, {newStart, newEnd});
+            }
+        }
     }
 }
 // @lc code=end
