35617c081d76f2eb8ab8d7725274af6513a1a57f
Kara X_X <jameszhan02@hotmail.com>
2026-03-30 15:32:11 -0700

    Update 212.word-search-ii.java

--- 212.word-search-ii.java
+++ 212.word-search-ii.java
@@ -6,68 +6,99 @@
 
 // @lc code=start
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 class Solution {
-
     int[][] directions = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+    PrefixTree wordTree = new PrefixTree();
+    // ⭐⭐⭐ avoid using HashSet
+    ArrayList<String> res = new ArrayList<>();
 
-    private boolean searchWord(int m, int n, char[][] board, String word, int idx) {
+    class PrefixTreeNode {
+        HashMap<Character, PrefixTreeNode> children = new HashMap<>();
+        boolean isWord = false;
+        // ⭐⭐⭐ save word in the node instead of build string dynamiclly.
+        String word = "";
+    }
 
-        if (board[m][n] != word.charAt(idx)) {
-            return false;
+    class PrefixTree {
+        PrefixTreeNode root = new PrefixTreeNode();
+
+        public void insert(String word) {
+            PrefixTreeNode cur = root;
+            for (int i = 0; i < word.length(); i++) {
+                if (cur.children.containsKey(word.charAt(i))) {
+                } else {
+                    cur.children.put(word.charAt(i), new PrefixTreeNode());
+                }
+                cur = cur.children.get(word.charAt(i));
+
+            }
+            cur.isWord = true;
+            cur.word = word;
         }
-        if (idx == word.length() - 1) {
-            return true;
-        }
-        char temp = board[m][n];
-        board[m][n] = '#';
-        int rows = board.length;
-        int cols = board[0].length;
-        for (int i = 0; i < directions.length; i++) {
-            int newM = m + directions[i][0];
-            int newN = n + directions[i][1];
-            if (newM >= 0 && newM < rows && newN >= 0 && newN < cols) {
-                if (board[newM][newN] != '#' &&
-                        searchWord(newM, newN, board, word, idx + 1)) {
-                    board[m][n] = temp;
-                    return true;
+
+        public boolean search(String word) {
+            // base on the current tree check if the word is exist on the path.
+            PrefixTreeNode currentNode = this.root;
+            for (int i = 0; i < word.length(); i++) {
+                char curChar = word.charAt(i);
+                if (currentNode.children.containsKey(curChar)) {
+                    currentNode = currentNode.children.get(curChar);
+                } else {
+                    return false;
                 }
             }
-
+            return true;
         }
-        board[m][n] = temp;
-        return false;
+
     }
 
     public List<String> findWords(char[][] board, String[] words) {
-        ArrayList<String> res = new ArrayList<>();
-        if (words.length == 0) {
-            return res;
+        // load all first letter in to a hash Set.
+        for (int i = 0; i < words.length; i++) {
+            wordTree.insert(words[i]);
         }
         int m = board.length;
         int n = board[0].length;
-
-        for (int x = 0; x < words.length; x++) {
-            boolean foundWord = false;
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (foundWord)
-                        break;
-                    if (words[x].charAt(0) == (board[i][j])) {
-                        if (foundWord)
-                            break;
-                        if (searchWord(i, j, board, words[x], 0)) {
-                            res.add(words[x]);
-                            foundWord = true;
-                        }
-                    }
-
-                }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // check how many word in tree can be build start with current Pos.
+                PrefixTreeNode currentNode = wordTree.root;
+                wordTreeSearch(board, i, j, currentNode);
             }
         }
-
         return res;
+    }
+
+    private void wordTreeSearch(char[][] board, int m, int n, PrefixTreeNode currentNode) {
+        if (m < 0 || n < 0 || m >= board.length || n >= board[0].length) {
+            return;
+        }
+        if (!currentNode.children.containsKey(board[m][n]))
+            return;
+        if (board[m][n] == '#')
+            return;
+        PrefixTreeNode newNode = currentNode.children.get(board[m][n]);
+        char temp = board[m][n];
+        board[m][n] = '#';
+        if (newNode.isWord) {
+            res.add(newNode.word);
+            newNode.isWord = false;
+        }
+        for (int d = 0; d < directions.length; d++) {
+            int newM = m + directions[d][0];
+            int newN = n + directions[d][1];
+            wordTreeSearch(board, newM, newN, newNode);
+        }
+        board[m][n] = temp;
+
+        // the key preformance booster. ⭐⭐⭐
+        if (newNode.children.isEmpty()) {
+            currentNode.children.remove(temp);
+        }
     }
 }
 // @lc code=end
